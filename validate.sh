7503d6e951f009f501385bcfda701e729cde0001
James Tauber <jtauber@jtauber.com>
2022-03-10 23:53:57 +0800

    source gloss x-flavour

--- validate.sh
+++ validate.sh
@@ -1,3 +1,4 @@
 node scripture-burrito-develop/code/validate.js metadata source-scripture-x/metadata.json
 node scripture-burrito-develop/code/validate.js metadata source-scripture-textTranslation/metadata.json
 node scripture-burrito-develop/code/validate.js metadata source-scripture-audioTranslation/metadata.json
+node scripture-burrito-develop/code/validate.js metadata source-gloss-x/metadata.json
