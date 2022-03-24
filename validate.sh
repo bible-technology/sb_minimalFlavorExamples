ed54393dad9f1bf81d4d24cb6714ff35ce86576f
James Tauber <jtauber@jtauber.com>
2022-03-24 22:28:54 +0800

    added minimal typeset scripture burrito

--- validate.sh
+++ validate.sh
@@ -1,4 +1,7 @@
 node scripture-burrito-develop/code/validate.js metadata source-scripture-x/metadata.json
 node scripture-burrito-develop/code/validate.js metadata source-scripture-textTranslation/metadata.json
 node scripture-burrito-develop/code/validate.js metadata source-scripture-audioTranslation/metadata.json
+node scripture-burrito-develop/code/validate.js metadata source-scripture-typesetScripture/metadata.json
+# node scripture-burrito-develop/code/validate.js metadata source-scripture-embossedBrailleScripture/metadata.json
+# node scripture-burrito-develop/code/validate.js metadata source-scripture-signLanguageVideoTranslation/metadata.json
 node scripture-burrito-develop/code/validate.js metadata source-gloss-x/metadata.json
